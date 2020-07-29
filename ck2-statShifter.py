314bf5a4fd0fa52ea15301e615af7f6ee4da4a32
jvbradley <jvbradley@users.noreply.github.com>
2020-07-29 15:44:44 -0700

    Update to Version 1.01.20200729a
     - Shortened script file name.
     - Corrected typos.
     - Removed unnecessary error handler.
     - Included accommodation for possibly missing fields 'piety' and 'wealth' and randomly created NPCs who had been invited to court.

---
+++ ck2-statShifter.py
@@ -0,0 +1,118 @@
+#! /usr/bin/python
+'''
+Crusader Kings II: Statistic Shifter
+
+This script facilitates editting of court members' statistics in Crusader Kings
+II saved game files.  It has been successfully tested on saved game files for
+version 3.3.3.0 of the game, though this formatting does appear consistent in
+other versions.
+
+This script assumes that you have installed Python 3.8.2 or a compatible
+version on your platform of choice.  To use this script, copy an NPC's
+information between the lines entitled 'att={ ...' and 'wealth={ ...', as
+demonstrated below:
+
+            att={2 2 8 2 2}
+            tr={10 85 77 188 83 }
+            rel="hellenic_pagan"
+            secret_religion="taoist"
+            cul="pictish"
+            dnt=1000104328
+            dna="qcilvebuwaj"
+            prp="wcviik00000000000000000000000000000000"
+            fer=0.200
+            health=4.000
+            title="title_cupbearer"
+            job="job_spymaster"
+            wealth=15.00000
+
+This script will accommodate can accommodate optional fields as well, such as
+titles, jobs, and consort status.  The script manages white space and line
+breaks in the data to ensure consistency with the current structure of the file.
+As this script makes no claims of warranty or guarantee, ALWAYS MAKE A BACK-UP
+OF YOUR SAVED GAMNE FILE BEFORE YOU MODIFY IT.
+'''
+def generateRandomAttributes():
+    # This function generates a set of five randomly generated attribute values
+    # between the values of 14 and 24 and then returns the data.  You can
+    # adjust the values 14 and 24.
+    import random
+    attOutput = '\t\t\tatt={'
+    for i in range(5):
+        attOutput += str(round(random.uniform(14,24))) + ' '
+        i += 1
+
+    attOutput = attOutput[0:-1] + '}'
+    return attOutput
+
+def statShifter():
+    import pyinputplus as pyip
+    import pyperclip
+    introText1 = 'Crusader Kings II: Statistic Shifter'
+    introText2 = 'Version 1.01.20200729a\n'
+    print('\n' + introText1.center(80))
+    print(introText2.center(80))
+
+    # Grab the attributes from memory.
+    attributeInput = pyperclip.paste()
+
+    # Make a list of the items from memory.
+    attributeInputByLine = attributeInput.split('\n')
+
+    # Check for missing default fields; add if necessary.
+    if '\t\t\tpiety=' not in attributeInputByLine:
+        attributeInputByLine.insert(12, '\t\t\tpiety=15.000\n')
+    if '\t\t\twealth=' not in attributeInputByLine[-1:]:
+        attributeInputByLine.append('\t\t\twealth=15.00000\n')
+
+    # These are the statistics that this script will modify.
+    modifyLines = ['att={', 'fer=', 'health=', 'prs=', 'piety=', 'wealth=']
+
+    # The script will leave these ones alone in this version.
+    skipLines = ['tr={', 'dnt=', 'dna="', 'prp="', 'lover=', 'title="', 'job="', 'player=', 'player_name="', 'rel=', 'gov=', 'consort=', 'consort_of=', 'cul=']
+
+    # This question will impact prestiege, piety, and wealth.
+    countyLordYN = pyip.inputYesNo(' * Is this character managing a county? ')
+
+    for line in attributeInputByLine:
+        # While iterating through the attribute lines, these lines will be
+        # blindly passed 'as-is' into the output.
+        for skip in skipLines:
+            if '\t\t\t' + skip in line:
+                newStatistics += line + '\n'
+        # End skip logic
+
+        # Itterate through the items, one line at a time.
+        for modifier in modifyLines:
+            # Check if this current line is a field that we want to adjust.
+            if '\t\t\t' + modifier in line:
+                if modifier == 'att={':
+                    newStatistics = str(generateRandomAttributes()) + '\n'
+                elif modifier == 'fer=':
+                    newStatistics += '\t\t\tfer=1.500\n'
+                elif modifier == 'health=':
+                    newStatistics += '\t\t\thealth=6.900\n'
+                elif modifier == 'prs=':
+                    if countyLordYN == 'yes':
+                        newStatistics += '\t\t\tprs=2750.000\n'
+                    elif countyLordYN == 'no':
+                        newStatistics += '\t\t\tprs=750.000\n'
+                elif modifier == 'piety=':
+                    if countyLordYN == 'yes':
+                        newStatistics += '\t\t\tpiety=1500.000\n'
+                    elif countyLordYN == 'no':
+                        newStatistics += '\t\t\tpiety=500.000\n'
+                elif modifier == 'wealth=':
+                    if countyLordYN == 'yes':
+                        newStatistics += '\t\t\twealth=125000.00000'
+                    elif countyLordYN == 'no':
+                        newStatistics += '\t\t\twealth=400.00000'
+            # This ends the logic for field adjustments.
+    # End iteration of statistics.
+
+    # Drop the updated statistics into memory, readying it for pasting.
+    pyperclip.copy(newStatistics)
+    print(' * Process complete.')
+    exit()
+
+statShifter()
